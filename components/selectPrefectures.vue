5a5743af58a13eaddbe794fc1d71134b1f85b089
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-05-31 13:51:08 +0900

    テストメソッド作成、予約画面作成途中

---
+++ components/selectPrefectures.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-select class="select" label="都道府県" :items="items" outlined>
+    </v-select
+    ><br />
+    <button type="button" @click="test">test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      items: [
+        "北海道",
+        "青森県",
+        "岩手県",
+        "宮城県",
+        "秋田県",
+        "山形県",
+        "福島県",
+        "茨城県",
+        "栃木県",
+        "群馬県",
+        "埼玉県",
+        "千葉県",
+        "東京都",
+        "神奈川県",
+        "新潟県",
+        "富山県",
+        "石川県",
+      ],
+    };
+  }, //end data
+
+  computed: {}, // end computed
+
+  methods: {
+    test() {
+      console.log("call");
+      this.$store.dispatch("test2");
+    },
+  }, // end methods
+};
+</script>
+
+<style scoped>
+.select {
+  width: 300px;
+}
+</style>
--- pages/reserveForm.vue
+++ pages/reserveForm.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <h4>予約者情報</h4>
+
+    氏名<v-text-field
+      class="name-field"
+      label="楽々太郎"
+      outlined
+    ></v-text-field>
+    かな<v-text-field
+      class="name2-field"
+      label="ラクラクタロウ"
+      outlined
+    ></v-text-field>
+    郵便番号<v-text-field
+      class="zipcode"
+      label="0000000"
+      outlined
+    ></v-text-field>
+    住所<selectPrefectures></selectPrefectures>
+  </div>
 </template>
 
 <script>
+import selectPrefectures from "../components/selectPrefectures.vue";
+
 export default {
   name: "reserveForm",
+  components: {
+    selectPrefectures,
+  },
   data() {
     return {};
   }, //end data
@@ -15,4 +39,10 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.name-field,
+.name2-field,
+.zipcode {
+  width: 300px;
+}
+</style>
