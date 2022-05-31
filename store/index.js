eadce2231f62168a302069b28f76c72ba224ece0
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-05-31 14:25:20 +0900

    mutationsにメソッドをセット

--- store/index.js
+++ store/index.js
@@ -4,7 +4,12 @@ import sample from "./modules/sample";
 import axios from "@nuxtjs/axios";
 
 Vue.use(Vuex);
-export const state = {};
+export const state = () => ({
+  // キーワード検索・ページ情報
+  pageInfo: {},
+  // キーワード検索・ホテル一覧
+  hotelList: [],
+});
 
 export const actions = {
   test2() {
@@ -17,18 +22,24 @@ export const actions = {
    * @param {*} keyword - キーワード
    */
   async getHotelList(context, keyword) {
-    console.log("call1");
     const response = await this.$axios.$get(
       `https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?applicationId=1098541415969458249&format=json&keyword=${keyword}`
     );
     console.dir(JSON.stringify(response));
-    const payload = response.data;
-    context.commit("showHotelList", payload);
+    context.commit("showHotelList", response);
   },
 }; // end actions
 
 export const mutations = {
-  
+  /**
+   * キーワード検索結果のホテルをstateに格納.
+   * @param {*} state
+   * @param {*} payload
+   */
+  showHotelList(state, payload) {
+    state.pageInfo = payload.pagingInfo;
+    state.hotelList = payload.hotels;
+  },
 };
 
 export const modules = {
