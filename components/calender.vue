f0d36a6941a388fa4def4b395dec6c51f92b8385
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-06-02 14:21:09 +0900

    カレンダーの表示周り修正

--- components/calender.vue
+++ components/calender.vue
@@ -3,14 +3,19 @@
     <v-col>
       <v-text-field
         @focus="dateFocus"
-        v-model="dates"
+        v-model="dateRangeText"
         label="Date range"
         prepend-icon="mdi-calendar"
         readonly
       ></v-text-field>
     </v-col>
     <v-card max-width="400" v-show="showCalender">
-      <v-date-picker class="calender" v-model="dates" range></v-date-picker>
+      <v-date-picker
+        class="calender"
+        v-model="dates"
+        range
+        @input="addDates"
+      ></v-date-picker>
       <v-card-actions>
         <v-btn text color="deep-purple accent-4" @click="closePopup"
           >閉じる</v-btn
@@ -21,6 +26,7 @@
 </template>
 
 <script>
+import { format } from "date-fns";
 export default {
   data() {
     return {
@@ -29,8 +35,27 @@ export default {
     };
   },
   methods: {
-    dateRangeText() {
-      return this.dates.join(" ~ ");
+    addDates() {
+      let array = [];
+      if (this.dates.length === 2) {
+        for (let i = 0; i < this.dates.length; i++) {
+          let candidateDate = new Date(this.dates[i]);
+          array.push(candidateDate);
+        }
+
+        // 日付を昇順に並び替える
+        array.sort(function (a, b) {
+          return a > b ? 1 : -1;
+        });
+
+        // 配列の中身を空に
+        this.dates.splice(0, this.dates.length);
+
+        for (let item of array) {
+          let formatString = format(item, "yyyy-MM-dd");
+          this.dates.push(formatString);
+        }
+      }
     },
     /**
      * カーソルが当たったときカレンダーを表示.
@@ -38,11 +63,21 @@ export default {
     dateFocus() {
       this.showCalender = true;
     },
+    /**
+     * モーダルを閉じる.
+     */
     closePopup() {
       this.showCalender = false;
     },
   },
-  computed: {},
+  computed: {
+    /**
+     * フォームの表示を整える.
+     */
+    dateRangeText() {
+      return this.dates.join(" ~ ");
+    },
+  },
 };
 </script>
 
