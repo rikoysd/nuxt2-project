df7dcd2ed691cc7a3e27b82893bf1fed0eff5005
Mmy26 <yu.b1994masamura@gmail.com>
2022-06-02 10:48:36 +0900

    store内API取得処理

--- pages/hotelDetail.vue
+++ pages/hotelDetail.vue
@@ -1,30 +1,84 @@
 <template>
   <div>
-    hotelList
+    <!-- {{ institutionInfo }} -->
+    <!-- <div v-for="(vacantInfo, i) of vacantList" :key="i">
+      {{ vacantInfo }}<br />
+      {{ vacantInfo.vacantRoom }}
+      <v-img :src="vacantInfo.roomImageUrl"></v-img>
+      <v-img :src="vacantInfo.roomThumbnailUrl"></v-img>
+      <v-img :src="vacantInfo.hotelImageUrl"></v-img>
+      <v-img :src="vacantInfo.hotelThumbnailUrl"></v-img>
+      <v-img :src="vacantInfo.hotelMapImageUrl"></v-img>
+    </div> -->
+
     <v-row>
-      <v-col cols="12"></v-col>
+      <v-col cols="12">
+        <v-carousel
+          cycle
+          height="400"
+          hide-delimiter-background
+          show-arrows-on-hover
+        >
+          <v-carousel-item v-for="(slide, i) in slides" :key="i">
+            <v-sheet :color="colors[i]" height="100%">
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="text-h2">
+                  <v-img align="center" width="100vw" :src="slide"></v-img>
+                </div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
     </v-row>
-    <!-- {{ name }}
-    <input type="text" v-model="name" />
-    <v-btn :onClick="nameChange">変更</v-btn> -->
   </div>
 </template>
 
 <script>
-// import store from "@/store/store.js";
 export default {
   data() {
-    // return { name: this.store.name };
+    return {
+      vacantList: [],
+      institutionInfo: "",
+      colors: [
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4",
+      ],
+      slides: [],
+    };
   },
   // methods: {
-  //   nameChange(name) {
-  //     this.store.dispatch(sample / changeName, name);
+  //   searchVacant() {
+  //     console.log("call1");
+  //     this.$store.dispatch("searchVacantList");
+  //     this.vacantList = this.$store.getters.getVacantList;
+  //     console.log(this.vacantList);
   //   },
   // },
-  // created() {
-  //   console.log("created", this.store.getName);
-  // },
+  async mounted() {
+    console.log("call1");
+    await this.$store.dispatch("searchVacantList");
+    this.vacantList = this.$store.getters.getVacantList;
+    console.log(
+      this.vacantList.hotels[0].hotel[0].hotelBasicInfo.roomImageUrl,
+      this.vacantList.hotels[0].hotel[0].hotelBasicInfo.hotelImageUrl
+    );
+    this.slides.push(
+      this.vacantList.hotels[4].hotel[0].hotelBasicInfo.roomImageUrl,
+      this.vacantList.hotels[4].hotel[0].hotelBasicInfo.hotelImageUrl
+    );
+    // await this.$store.dispatch("searchInstitution");
+    // this.institutionInfo = this.$store.getters.getInstitutitonInfo;
+    // console.log(this.institutionInfo);
+  },
 };
 </script>
 
-<style></style>
+<style>
+v-img {
+  width: 10px;
+}
+</style>
--- store/index.js
+++ store/index.js
@@ -1,19 +1,74 @@
 import Vuex from "vuex";
 import Vue from "vue";
 import sample from "./modules/sample";
+import searchInstitution from "./modules/searchInstitution";
+import axios from "axios";
 
 Vue.use(Vuex);
-export const state = {};
+export const state = {
+  // 空室情報
+  // hotels[i].hotel[0]:基本情報
+  //          .hotel[1]:詳細情報
+  //          .hotel[2]:予約情報(料金)
+  //          .hotel[3]:部屋情報(プラン１)
+  //          .hotel[4]:部屋情報(プラン２)
+  vacantList: [],
+};
 
 export const actions = {
   test2() {
     console.log("call2");
     this.dispatch("sample/getData", { root: true });
   },
+  /**
+   * 空室検索.
+   */
+  async searchVacantList() {
+    console.log("call");
+    const vacantResponce = await axios.get(
+      `https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?applicationId=1098541415969458249&format=json&largeClassCode=japan&middleClassCode=akita&smallClassCode=tazawa&checkinDate=2022-12-01&checkoutDate=2022-12-02&adultNum=2&responseType=large`
+    );
+    console.dir("response" + JSON.stringify(vacantResponce.data.hotels));
+    this.commit("setVacantList", vacantResponce.data.hotels);
+  },
+  /**
+   * 施設検索(モジュール:searchInstitution).
+   */
+  searchInstitution() {
+    // console.log("call");
+    this.dispatch("searchInstitution/searchInstitution", { root: true });
+  },
 }; // end actions
 
-export const mutations = {};
-
+export const mutations = {
+  /**
+   * 空室検索の結果をstateにセット.
+   */
+  setVacantList(state, payload) {
+    state.vacantList = { hotels: payload };
+    // console.log(state.vacantList);
+  },
+};
+export const getters = {
+  /**
+   * @param {*} state - ステート
+   * @returns - 空室リスト
+   */
+  getVacantList(state) {
+    return state.vacantList;
+  },
+  /*
+   * @param {*} state - ステート
+   * @returns - 施設情報
+   */
+  getInstitutitonInfo(state) {
+    return state.searchInstitution;
+  },
+};
+/**
+ * vacantListの取得.
+ */
 export const modules = {
   sample,
+  searchInstitution,
 };
