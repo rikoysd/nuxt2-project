0975b44bbd610816b6ce14b9d799a521c1e5c293
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-06-02 12:29:31 +0900

    メソッド設置

--- pages/reserveFinished.vue
+++ pages/reserveFinished.vue
@@ -3,7 +3,7 @@
     <div class="msg">
       <h2>ご予約ありがとうございました。</h2>
       <p>予約履歴はマイページからご確認いただけます。</p>
-      <v-btn>トップへ戻る</v-btn>
+      <v-btn @click="moveToTop">トップへ戻る</v-btn>
     </div>
   </div>
 </template>
@@ -17,7 +17,11 @@ export default {
 
   computed: {}, // end computed
 
-  methods: {}, // end methods
+  methods: {
+    moveToTop() {
+      this.$router.push("/");
+    },
+  }, // end methods
 };
 </script>
 
