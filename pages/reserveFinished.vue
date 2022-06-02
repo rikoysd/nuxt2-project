2f3970910d2d213afbe54e629b4943a8eac04d46
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-06-02 12:28:17 +0900

    レイアウト調整等

---
+++ pages/reserveFinished.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="msg">
+      <h2>ご予約ありがとうございました。</h2>
+      <p>予約履歴はマイページからご確認いただけます。</p>
+      <v-btn>トップへ戻る</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "reserveFinished",
+  data() {
+    return {};
+  }, //end data
+
+  computed: {}, // end computed
+
+  methods: {}, // end methods
+};
+</script>
+
+<style scoped></style>
