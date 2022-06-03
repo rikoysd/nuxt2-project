df31493a4e972b3578c95fa8ed9671255678ee3b
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-06-03 19:56:13 +0900

    ブラウザタブのタイトル変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from "vuetify/es5/util/colors";
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - nuxt2-project",
+    titleTemplate: "宿泊予約サイト",
     title: "nuxt2-project",
     htmlAttrs: {
       lang: "en",
