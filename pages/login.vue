e5e3e7bf34e5d90678ea4f39449ae3ed5d8d334c
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-06-03 11:24:47 +0900

    エラー処理（途中）

---
+++ pages/login.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    メールアドレス<span>{{ mailAddressError }}</span
+    ><v-text-field
+      class="mailAddress"
+      label="rakuraku@example.jp"
+      v-model="mailAddress"
+      outlined
+    ></v-text-field>
+    パスワード<span>{{ passwordError }}</span
+    ><v-text-field
+      class="password"
+      label="rakus12345（8文字以上で設定してください）"
+      v-model="password"
+      outlined
+      type="password"
+    ></v-text-field>
+    <v-btn class="login-info" color="primary" @click="login">ログイン</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // メールアドレス
+      mailAddress: "",
+      // パスワード
+      password: "",
+      // メールアドレスのエラー
+      mailAddressError: "",
+      // パスワードのエラー
+      passwordError: "",
+      // エラーチェック
+      errorCheck: false,
+      // エラーリスト
+      errors: [],
+    };
+  },
+  methods: {
+    /**
+     * ログインする.
+     */
+    login() {
+      // エラーリストの初期化
+      this.errors = [];
+
+      // メールアドレスのエラー
+      if (this.mailAddress === "") {
+        this.mailAddressError = "メールアドレスを入力してください";
+        this.errorCheck = true;
+      } else {
+        this.mailAddressError = "";
+        this.errorCheck = false;
+      }
+      this.errors.push(this.errorCheck);
+
+      // パスワードのエラー
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style scoped>
+span {
+  color: red;
+}
+</style>
