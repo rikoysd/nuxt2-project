d64f99e97b975e9edd21e1203484b133a2c75d85
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-06-03 17:15:58 +0900

    日付選択微修正

--- components/calender.vue
+++ components/calender.vue
@@ -8,19 +8,21 @@
         readonly
         outlined
       ></v-text-field>
-      <v-card max-width="400" v-show="showCalender">
-        <v-date-picker
-          class="calender"
-          v-model="dates"
-          range
-          @input="addDates"
-        ></v-date-picker>
-        <v-card-actions>
-          <v-btn text color="deep-purple accent-4" @click="closePopup"
-            >閉じる</v-btn
-          >
-        </v-card-actions>
-      </v-card>
+      <v-expand-transition>
+        <v-card max-width="400" v-show="showCalender">
+          <v-date-picker
+            class="calender"
+            v-model="dates"
+            range
+            @input="addDates"
+          ></v-date-picker>
+          <v-card-actions>
+            <v-btn text color="deep-purple accent-4" @click="closePopup"
+              >閉じる</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-expand-transition>
     </v-col>
   </div>
 </template>
