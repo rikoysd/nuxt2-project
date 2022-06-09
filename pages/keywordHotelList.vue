f9d6192aec95c018a9c267022a2dfca55863da01
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-06-09 09:37:15 +0900

    css微調整

--- pages/keywordHotelList.vue
+++ pages/keywordHotelList.vue
@@ -13,89 +13,93 @@
       <keywords @search="searchKeyword"></keywords>
       <!-- 検索結果ページ（初期表示） -->
       <div class="error-flag">{{ getErrorFlag }}</div>
-      <div v-show="showResult">
-        <!-- ページネーション -->
-        <div class="text-center">
-          <v-pagination
-            v-model="page"
-            :length="5"
-            @input="getNumber"
-          ></v-pagination>
-        </div>
-        <!-- 検索結果カンマ区切り -->
-        <div class="record-count">
-          対象施設：{{ Number(getPageInfo.recordCount).toLocaleString() }}件
-        </div>
-        <!-- カード -->
-        <v-row>
-          <v-col
-            class="d-flex justify-center"
-            v-for="(hotel, index) of getHotelList"
-            v-bind:key="index"
-          >
-            <v-card class="card" max-width="399">
-              <v-img
-                class="white--text align-end"
-                height="160px"
-                v-bind:src="hotel.hotel[0].hotelBasicInfo.hotelImageUrl"
-              >
-              </v-img>
-              <v-card-text class="text--primary">
-                <div class="hotel-name">
-                  {{ hotel.hotel[0].hotelBasicInfo.hotelName }}
-                </div>
-                <div class="pb-0 sub-title">
-                  {{ hotel.hotel[2].hotelDetailInfo.areaName }} [全{{
-                    hotel.hotel[3].hotelFacilitiesInfo.hotelRoomNum
-                  }}室]
-                  <star-rating
-                    v-bind:increment="0.01"
-                    v-bind:max-rating="5"
-                    v-bind:rating="hotel.hotel[0].hotelBasicInfo.reviewAverage"
-                    inactive-color="#000"
-                    active-color="#fa8000"
-                    v-bind:star-size="15"
-                    v-bind:read-only="true"
-                  >
-                  </star-rating>
-                </div>
-                <div class="description">
-                  {{ hotel.hotel[0].hotelBasicInfo.hotelSpecial }}
-                </div>
-                <div class="min-charge">
-                  最安値<span
-                    >&nbsp;{{
-                      Number(
-                        hotel.hotel[0].hotelBasicInfo.hotelMinCharge
-                      ).toLocaleString()
-                    }}&nbsp;</span
-                  >
-                  円(税込)
-                </div>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn
-                  color="orange"
-                  text
-                  @click="
-                    showHotelDetail(hotel.hotel[0].hotelBasicInfo.hotelNo)
-                  "
-                  >詳細を見る</v-btn
+      <div class="d-flex justify-center">
+        <div v-show="showResult" class="result">
+          <!-- ページネーション -->
+          <div class="text-center">
+            <v-pagination
+              v-model="page"
+              :length="5"
+              @input="getNumber"
+            ></v-pagination>
+          </div>
+          <!-- 検索結果カンマ区切り -->
+          <div class="record-count">
+            対象施設：{{ Number(getPageInfo.recordCount).toLocaleString() }}件
+          </div>
+          <!-- カード -->
+          <v-row>
+            <v-col
+              class="d-flex justify-center"
+              v-for="(hotel, index) of getHotelList"
+              v-bind:key="index"
+            >
+              <v-card class="card" max-width="399">
+                <v-img
+                  class="white--text align-end"
+                  height="160px"
+                  v-bind:src="hotel.hotel[0].hotelBasicInfo.hotelImageUrl"
                 >
-                <favorite-button
-                  :hotelNum="hotel.hotel[0].hotelBasicInfo.hotelNo"
-                ></favorite-button>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-        <!-- ページネーション -->
-        <div class="text-center">
-          <v-pagination
-            v-model="page"
-            :length="5"
-            @input="getNumber"
-          ></v-pagination>
+                </v-img>
+                <v-card-text class="text--primary">
+                  <div class="hotel-name">
+                    {{ hotel.hotel[0].hotelBasicInfo.hotelName }}
+                  </div>
+                  <div class="pb-0 sub-title">
+                    {{ hotel.hotel[2].hotelDetailInfo.areaName }} [全{{
+                      hotel.hotel[3].hotelFacilitiesInfo.hotelRoomNum
+                    }}室]
+                    <star-rating
+                      v-bind:increment="0.01"
+                      v-bind:max-rating="5"
+                      v-bind:rating="
+                        hotel.hotel[0].hotelBasicInfo.reviewAverage
+                      "
+                      inactive-color="#000"
+                      active-color="#fa8000"
+                      v-bind:star-size="15"
+                      v-bind:read-only="true"
+                    >
+                    </star-rating>
+                  </div>
+                  <div class="description">
+                    {{ hotel.hotel[0].hotelBasicInfo.hotelSpecial }}
+                  </div>
+                  <div class="min-charge">
+                    最安値<span
+                      >&nbsp;{{
+                        Number(
+                          hotel.hotel[0].hotelBasicInfo.hotelMinCharge
+                        ).toLocaleString()
+                      }}&nbsp;</span
+                    >
+                    円(税込)
+                  </div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn
+                    color="orange"
+                    text
+                    @click="
+                      showHotelDetail(hotel.hotel[0].hotelBasicInfo.hotelNo)
+                    "
+                    >詳細を見る</v-btn
+                  >
+                  <favorite-button
+                    :hotelNum="hotel.hotel[0].hotelBasicInfo.hotelNo"
+                  ></favorite-button>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+          <!-- ページネーション -->
+          <div class="text-center">
+            <v-pagination
+              v-model="page"
+              :length="5"
+              @input="getNumber"
+            ></v-pagination>
+          </div>
         </div>
       </div>
     </div>
@@ -226,10 +230,6 @@ export default {
   transition: all 0.3s ease 0s;
 }
 
-.card:hover {
-  opacity: 0.7;
-}
-
 .description {
   margin-top: 5px;
   font-size: 12px;
@@ -255,6 +255,10 @@ export default {
 
 .record-count {
   margin-bottom: 10px;
+}
+
+.result {
+  width: 90%;
 }
 
 .sub-title {
